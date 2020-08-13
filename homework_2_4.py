84ed18c05d6e345dc28f61eee3dc63630dba1a08
YuriiShp <69315230+YuriiShp@users.noreply.github.com>
2020-08-13 19:21:36 +0300

    Update homework_2_4.py

--- homework_2_4.py
+++ homework_2_4.py
@@ -5,3 +5,11 @@ if dict_a.get(inp_key, 'none') != 'none':
     print(dict_a.get(inp_key))
 else:
     print('key does not exist')
+    
+# Correct
+dict_a = {'name': 'Oleg', 'age': 25, 'education': 'higher'}
+inp_key = input('Input key: ')
+if dict_a.get(inp_key):
+    print(dict_a.get(inp_key))
+else:
+    print('key does not exist')
